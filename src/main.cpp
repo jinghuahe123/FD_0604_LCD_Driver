70eeafd3d4097cf45ef79d5d5fd247a67095c162
jinghuahe123 <hej.09@outlook.com>
2026-01-01 11:11:34 +0000

    Optimised multiplexing cycle to be faster

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 #include "DisplayDriver.h"
 
 const int gnd[2] = {2, 3};
@@ -21,39 +20,9 @@ void clear() {
   }
 }
 
-void writeDigit(int number) {
+void writeArray(int number, int interval, bool clock) {
+  int out[2][15];
 
-  getDisplayDigit(number, digitData);
-
-  clear();
-  digitalWrite(gnd[0], LOW);
-  digitalWrite(gnd[1], HIGH);
-  for (int i=0; i<15; i++) {
-    digitalWrite(vcc[i], digitData[0][i]);
-  }
-  delay(1);
-  digitalWrite(gnd[0], HIGH);
-  digitalWrite(gnd[1], LOW);
-  for (int i=0; i<15; i++) {
-    digitalWrite(vcc[i], digitData[1][i]);
-  }
-  delay(1);
-}
-
-void initDisplay(int a, int b, int c, int d) {
-
-  // display ones digit
-  writeDigit(d);
-
-  // display tens digit
-  writeDigit(c+10);
-
-  // display hundreds digit
-  writeDigit(b+20);
-  
-}
-
-void writeDisplay(int number, int interval) {
   int arr[4] = {};
   // Parse the number into the array
   for (int i = 3; i >= 0; i--) {
@@ -61,11 +30,47 @@ void writeDisplay(int number, int interval) {
     number /= 10;         // Remove the last digit from the number
   }
 
+  int arr0[2][15];
+  int arr1[2][15];
+  int arr2[2][15];
+  int arr3[2][15];
+
+  int arr4[2][15] = {0};
+
+  getDisplayDigit(arr[0]+30, arr0);
+  getDisplayDigit(arr[1]+20, arr1);
+  getDisplayDigit(arr[2]+10, arr2);
+  getDisplayDigit(arr[3], arr3);
+
+  if (clock) getDisplayDigit(34, arr4);
+
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 15; j++) {
+        // Use bitwise OR to combine the values from all four arrays
+        out[i][j] = arr0[i][j] | arr1[i][j] | arr2[i][j] | arr3[i][j] | arr4[i][j];
+    }
+  }
+
   unsigned long currentMillis = millis();
   previousMillis = currentMillis;
   while (currentMillis - previousMillis <= interval) {
+    if (interval == 0) currentMillis = 0;
     currentMillis = millis();
-    initDisplay(arr[0], arr[1], arr[2], arr[3]);
+
+    clear();
+    digitalWrite(gnd[0], LOW);
+    digitalWrite(gnd[1], HIGH);
+    for (int i=0; i<15; i++) {
+      digitalWrite(vcc[i], out[0][i]);
+    }
+    delay(1);
+    digitalWrite(gnd[0], HIGH);
+    digitalWrite(gnd[1], LOW);
+    for (int i=0; i<15; i++) {
+      digitalWrite(vcc[i], out[1][i]);
+    }
+    delay(1);
   }
 }
 
@@ -78,7 +83,7 @@ void setup() {
 
 void loop() {
   for (int i=0; i<4000; i++) {
-    writeDisplay(i, interval);
+    writeArray(i, interval, 0);
   }
 
 }
