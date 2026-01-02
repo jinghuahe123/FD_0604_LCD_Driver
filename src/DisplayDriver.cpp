73bdd5652dca01fc73625259de6156bbd42433d9
jinghuahe123 <hej.09@outlook.com>
2026-01-02 14:56:18 +0000

    Added support for dual 74HC595 Shift Regsisters

--- src/DisplayDriver.cpp
+++ src/DisplayDriver.cpp
@@ -1,78 +1,85 @@
 #include <DisplayDriver.h>
 #include <Digit_Patterns.h>
 
-DisplayDriver::DisplayDriver(const int* gnds, const int* vccs, bool npn_toggle) {
-  /**
-   * @param gnds        The GND pins to address.
-   * @param vccs        The VCC pins to address.
-   * @param npn         Toggle output polarity if the NPN-transistor is connected.
-   */
-
+/**
+ * @param gnds        The GND pins to address.
+ * @param vccs        The VCC pins to address.
+ * @param npn         Toggle output polarity if the NPN-transistor is connected.
+ */
+DisplayDriver::DisplayDriver(const uint8_t* gnds, const uint8_t* pins, bool npn_toggle) {
   gnd = gnds;
-  vcc = vccs;
+  latchPin = pins[0];
+  clockPin = pins[1];
+  dataPin = pins[2];
   npn = npn_toggle;
+
+  clear();
 }
 
-void DisplayDriver::getDisplayDigit(int digit, int16_t (&output)[2]) {
+void DisplayDriver::getDisplayDigit(uint8_t digit, uint16_t (&output)[2]) {
   memcpy_P(output, &display[digit], sizeof(display[digit]));
 }
 
+/**
+ * @details   Clears the display.
+ */
 void DisplayDriver::clear() {
   for (int i=0; i<2; i++) {
     pinMode(gnd[i], OUTPUT);
     digitalWrite(gnd[i], HIGH);
   }
 
-  for (int i=0; i<15; i++) {
-    pinMode(vcc[i], OUTPUT);
-    digitalWrite(vcc[i], LOW);
-  }
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
 }
 
-void DisplayDriver::writePins(unsigned long interval, int16_t* displayPins) {
-  /**
-   * @param interval      The time the number should be displayed for.
-   * @param displayPins   The pointer array of to-display LED's on/off pins. 
-   */
+/**
+ * @details       Writes dual shift registers with display data
+ * @param data    Entire two bytes of data for each ground pin. 
+ */
+void DisplayDriver::writeShiftRegister(uint16_t data) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, data);
+  shiftOut(dataPin, clockPin, LSBFIRST, data >> 8);
+  digitalWrite(latchPin, HIGH);
+}
 
+/**
+ * @details             Multiplexes the display.
+ * @param interval      The time the number should be displayed for.
+ * @param displayPins   The pointer array of to-display LED's on/off pins. 
+ */
+void DisplayDriver::writePins(unsigned long interval, uint16_t* displayPins) {
   unsigned long currentMillis = millis();
   previousMillis = currentMillis;
   while (currentMillis - previousMillis <= interval) {
     if (interval == 0) currentMillis = 0;
     currentMillis = millis();
 
-    int16_t pins0 = displayPins[0];
-    int16_t pins1 = displayPins[1];
-
     //clear();
 
     digitalWrite(gnd[0], npn ? HIGH : LOW); // Invert GND output if NPN is connected.
     digitalWrite(gnd[1], npn ? LOW : HIGH);
-    for (int i=14; i>=0; i--) {
-      digitalWrite(vcc[i], pins0&1);
-      pins0 = pins0 >> 1;
-    }
-    _delay_ms(1);
+    writeShiftRegister(displayPins[0]);
+    _delay_ms(MULTIPLEX_SPEED);
     
     digitalWrite(gnd[0], npn ? LOW : HIGH);
     digitalWrite(gnd[1], npn ? HIGH : LOW);
-    for (int i=14; i>=0; i--) {
-      digitalWrite(vcc[i], pins1&1);
-      pins1 = pins1 >> 1;
-    }
-    _delay_ms(1);
+    writeShiftRegister(displayPins[1]);
+    _delay_ms(MULTIPLEX_SPEED);
   }
 }
 
-void DisplayDriver::writeArray(int number, unsigned long interval, bool clock) {
-  /**
-   * @param number      The desired display number.
-   * @param interval    The time the number should be displayed for.
-   * @param clock       Toggle the clock LEDs. 
-   */
-  
+/**
+ * @details           Parses each individual display number together.
+ * @param number      The desired display number.
+ * @param interval    The time the number should be displayed for.
+ * @param clock       Toggle the clock LEDs. 
+ */
+void DisplayDriver::writeArray(uint16_t number, unsigned long interval, bool clock) {
   int arr[4] = {0};
-  int16_t arr0[2], arr1[2], arr2[2], arr3[2], arr4[2], out[2] = {0};
+  uint16_t arr0[2], arr1[2], arr2[2], arr3[2], arr4[2], out[2] = {0};
   
   // Parse the number into the array
   for (int i = 3; i >= 0; i--) {
@@ -96,13 +103,15 @@ void DisplayDriver::writeArray(int number, unsigned long interval, bool clock) {
   DisplayDriver::writePins(interval, out);
 }
 
+/**
+ * @details           Parses the null display.
+ * @param interval    The time the number should be displayed for.
+ * @param clock       Toggle the clock LEDs. 
+ */
 void DisplayDriver::writeNull(unsigned long interval, bool clock) {
-  /**
-   * @param interval    The time the number should be displayed for.
-   * @param clock       Toggle the clock LEDs. 
-   */
+  
 
-  int16_t null_digits[2], clock_digits[2], out[2];
+  uint16_t null_digits[2], clock_digits[2], out[2];
   
   getDisplayDigit(35, null_digits);
   if (clock) getDisplayDigit(34, clock_digits);
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,22 @@
 #include <Arduino.h>
 #include <DisplayDriver.h>
 
-/*
-  --ARDUINO PIN-- | --DISPLAY PIN--
-        2                 1
-        3                 2
-        4                 6
-        5                 7
-        6                 8
-        7                 9
-        8                 10
-        9                 12
-        10                13
-        11                15
-        12                16
-        13                17
-        A0                18
-        A1                19 
-        A2                20
-        A3                21
-        A4                30
-*/
-
-const int gnd[2] = {2, 3}; // first two pins of display in order of connection
-const int vcc[15] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A4}; // the rest of the display pins in order
+const uint8_t gnd[2] = {2, 3}; // first two pins of display in order of connection
+const uint8_t pins[3] = {4, 5, 6}; // order of latchpin, clockpin, datapin
 const unsigned long interval = 100; // delay time
 
-DisplayDriver myDisplay(gnd, vcc, true);
+DisplayDriver myDisplay(gnd, pins, true);
 
 int main(void) {
   init();
   //initVariant();
 
   Serial.begin(115200);
-  myDisplay.clear();
+  //myDisplay.clear();
 
   for(;;) {
     for (int i=0; i<4000; i++) {
       myDisplay.writeArray(i, interval, i%2);
-    }
+    }   
   }
 }
