735a659db7d00695e222a8a9f633dd42e56b0cee
jinghuahe123 <109895279+jinghuahe123@users.noreply.github.com>
2026-01-12 17:05:51 +0000

    Added MINIMAL_WIRING option

--- src/DisplayDriver.h
+++ src/DisplayDriver.h
@@ -5,6 +5,9 @@
 
 #define MULTIPLEX_SPEED 4
 
+#define NORMAL_WIRING 0
+#define MINIMAL_WIRING 1
+
 class DisplayDriver {
     private:
         const uint8_t* gnd;
@@ -12,6 +15,7 @@ class DisplayDriver {
         uint8_t clockPin;
         uint8_t dataPin;
         bool npn;
+        uint8_t wiring_style;
         unsigned long previousMillis = 0;
 
         void getDisplayDigit(uint8_t digit, uint16_t (&output)[2]);
@@ -20,7 +24,8 @@ class DisplayDriver {
 
 
     public:
-        DisplayDriver(const uint8_t* gnds, const uint8_t* pins, bool npn);
+        DisplayDriver(const uint8_t* gnds, const uint8_t* pins, bool npn_toggle);
+        DisplayDriver(const uint8_t* pins, bool npn_toggle);
 
         void clear();
         void writeArray(uint16_t number, unsigned long interval, bool clock);
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 #include <DisplayDriver.h>
 
+#define USE_MINIMAL_WIRING
+
 const uint8_t gnd[2] = {2, 3}; // first two pins of display in order of connection
 const uint8_t pins[3] = {4, 5, 6}; // order of latchpin, clockpin, datapin
 const unsigned long interval = 100; // delay time
 
+#ifdef USE_MINIMAL_WIRING
+DisplayDriver myDisplay(pins, true);
+#else
 DisplayDriver myDisplay(gnd, pins, true);
+#endif
+
 
 int main(void) {
   init();
@@ -17,7 +24,7 @@ int main(void) {
   for(;;) {
     for (int i=0; i<4000; i++) {
       myDisplay.writeArray(i, interval, i%2);
-    }   
+    }
   }
   
   return 0;
