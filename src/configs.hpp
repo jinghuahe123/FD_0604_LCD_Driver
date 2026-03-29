22c9438e2e1e3d7a828a292e4de53e722d9cce48
jinghuahe123 <109895279+jinghuahe123@users.noreply.github.com>
2026-03-29 18:05:55 +0100

    Add showInfo method for hardware report.

--- src/configs.hpp
+++ src/configs.hpp
@@ -1,8 +1,6 @@
 #include "DisplayController_FD0604.hpp"
 
-#define PORTMANIP
-
-DisplayDriver_FD0604::DriverParams_MinimalWiring minimalDisplay = {
+DisplayDriver_FD0604::DriverParams_MinimalWiring minimalDisplayParams = {
     1,          // transistor enabled
 
     6,          // arduino latchpin
@@ -10,7 +8,7 @@ DisplayDriver_FD0604::DriverParams_MinimalWiring minimalDisplay = {
     8           // arduino datapin
 };
 
-DisplayDriver_FD0604::DriverParams_DIRECTPORT_MinimalWiring minimalPortDisplay = {
+DisplayDriver_FD0604::DriverParams_DIRECTPORT_MinimalWiring minimalPortDisplayParams = {
     1,          // transistor enabled
 	
 	&DDRD,      // latchpin DDRx register
@@ -27,7 +25,11 @@ DisplayDriver_FD0604::DriverParams_DIRECTPORT_MinimalWiring minimalPortDisplay =
 };
 
 
-DisplayController_FD0604::DisplayController_FD0604_Parameters displayParameters = {
+DisplayController_FD0604::DisplayController_FD0604_Parameters controllerParams = {
+    0,          // EEPROM address to start writing writing from
+    6,          // uint32_t for sequence number (for wear levelling) + uint16_t for number
+    170,        // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
+
     100,        // number of ms to delay between counting intervals
 
     A7,         // temperature sensor  pin
@@ -43,9 +45,9 @@ DisplayController_FD0604::DisplayController_FD0604_Parameters displayParameters 
 };
 
 
-const uint16_t BASE_ADDR = 0;            // EEPROM address to start writing writing from
-const uint8_t SLOT_SIZE = 6;            // uint32_t for sequence number (for wear levelling) + uint16_t for number
-const uint16_t NUM_SLOTS = 170;          // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
+//const uint16_t BASE_ADDR = 0;            // EEPROM address to start writing writing from
+//const uint8_t SLOT_SIZE = 6;            // uint32_t for sequence number (for wear levelling) + uint16_t for number
+//const uint16_t NUM_SLOTS = 170;          // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
 
 //const uint8_t gnd[2] = {2, 3};      // first two pins of display in order of connection
 //const uint8_t pins[3] = {6, 7, 8};  // order of latchpin, clockpin, datapin
