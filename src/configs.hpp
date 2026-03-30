2a526cc323f6032b28c0c57d1986d98642bf21b5
jinghuahe123 <hej.09@outlook.com>
2026-03-30 12:24:55 +0100

    Add Runtime configurable parameters.

--- src/configs.hpp
+++ src/configs.hpp
@@ -26,20 +26,20 @@ DisplayDriver_FD0604::DriverParams_DIRECTPORT_MinimalWiring minimalPortDisplayPa
 
 
 DisplayController_FD0604::DisplayController_FD0604_Parameters controllerParams = {
-    0,          // EEPROM address to start writing writing from
+    16,         // EEPROM address to start writing writing from
     6,          // uint32_t for sequence number (for wear levelling) + uint16_t for number
-    170,        // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
+    160,        // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
 
-    100,        // number of ms to delay between counting intervals
+    0,          // EEPROM Address that stores the delay between counting intervals 
 
     A7,         // temperature sensor  pin
     10000.0,    // temperature sensor accompanying resistor
-    256,        // temperature sensor update interval
+    4,          // EEPROM address that stores the delay between the temperature reading updating
     0,          // enable serial output for temperature sensor
 
     A6,         // raw input pin
-    196,        // raw input update interval
+    8,          // EEPROM address that stores the delay between the raw input reading updating 
 
-    E2END,      // last EEPROM address for storing display orientation data
+    12,         // EEPROM address for storing display orientation data
     128         // How many numbers to search for in history. Don't set too large otherwise MCU will run out of RAM. 
 };
