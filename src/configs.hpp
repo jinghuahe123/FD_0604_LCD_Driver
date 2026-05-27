d1be461f086f135436a0cbf6808a507c1d381814
jinghuahe123 <hej.09@outlook.com>
2026-05-27 13:51:11 +0100

    More verbose configs.hpp

--- src/configs.hpp
+++ src/configs.hpp
@@ -19,38 +19,38 @@
 constexpr char version[] PROGMEM =  "FD_0604 LED Display v0.1.28";
 
 DisplayDriver_FD0604::DriverParams_DIRECTPORT_MinimalWiring displayParams = {
-    1,          // transistor enabled
+    .npn_transistor_enable = 1,
 	
-	&DDRD,      // latchpin DDRx register
-	&PORTD,     // latchpin PORTx register
-	6,          // latchpin physical pin (on register)
+	.DDRx_latchPin = &DDRD,             // latchpin data direction register
+	.PORTx_latchPin = &PORTD,           // latchpin port register
+	.PIN_latchPin = 6,                  // latchpin physical pin (on register)
 	
-	&DDRD,      // clockpin DDRx register
-	&PORTD,     // clockpin PORTx register
-	7,          // clockpin physical pin (on register)
+	.DDRx_clockPin = &DDRD,             // clockpin data direction register   
+	.PORTx_clockPin = &PORTD,           // clockpin port register
+	.PIN_clockPin = 7,                  // clockpin physical pin (on register)
 	
-	&DDRB,      // datapin DDRx register
-	&PORTB,     // datapin PORTx register
-	0,          // datapin physical pin (on register)
+	.DDRx_dataPin = &DDRB,              // datapin data direction register
+	.PORTx_dataPin = &PORTB,            // datapin port register
+	.PIN_dataPin = 0,                   // datapin physical pin (on register)
 };
 
 
 DisplayController_FD0604::DisplayController_FD0604_Parameters controllerParams = {
-    0x0040,     // EEPROM address to start writing writing from
-    6,          // uint32_t for sequence number (for wear levelling) + uint16_t for number
-    160,        // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
+    .BASE_ADDR = 0x0040,     // EEPROM address to start writing writing from
+    .SLOT_SIZE = 6,          // uint32_t for sequence number (for wear levelling) + uint16_t for number
+    .NUM_SLOTS = 160,        // maximum number of slots to use for wear levelling (SLOT_SIZE*NUM_SLOTS must < EEPROM.size())
 
-    0x0020,     // EEPROM Address that stores the delay between counting intervals 
+    .countingIntervalAddress = 0x0020,     // EEPROM Address that stores the delay between counting intervals 
 
-    A7,         // temperature sensor  pin
-    10000.0,    // temperature sensor accompanying resistor
-    0x0022,     // EEPROM address that stores the delay between the temperature reading updating
-    0x0024,     // EEPROM address for enable serial output for temperature sensor
+    .temperaturePin = A7,                           // temperature sensor  pin
+    .resistorValue = 10000.0,                       // temperature sensor accompanying resistor
+    .temperatureUpdateIntervalAddress = 0x0022,     // EEPROM address that stores the delay between the temperature reading updating
+    .temperatureSerialEnabledAddress = 0x0024,      // EEPROM address for enable serial output for temperature sensor
 
-    A6,         // raw input pin
-    0x0026,     // EEPROM address that stores the delay between the raw input reading updating 
-    0x0028,     // EEPROM address for enable serial output for raw input
+    .rawInputPin = A6,                          // raw input pin
+    .rawInputUpdateIntervalAddress = 0x0026,    // EEPROM address that stores the delay between the raw input reading updating 
+    .rawInputSerialEnabledAddress = 0x0028,     // EEPROM address for enable serial output for raw input
 
-    0x002A,     // EEPROM address for storing display orientation data
-    0x002C      // EEPROM address for history recall depth
+    .displayOrientationAddress = 0x002A,     // EEPROM address for storing display orientation data
+    .numHistoryAddress = 0x002C              // EEPROM address for history recall depth
 };
